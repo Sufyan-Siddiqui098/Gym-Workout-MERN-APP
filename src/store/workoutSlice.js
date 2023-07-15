321173b234b10ecf0b0cb7d5c85e3d845e53b1fb
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-15 22:53:57 +0500

    add env variable to avoid rewrite backend hosting url

--- src/store/workoutSlice.js
+++ src/store/workoutSlice.js
@@ -1,13 +1,15 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
 
+let host = process.env.REACT_APP_HOST
+
 export const fetchWorkouts = createAsyncThunk("fetchWorkouts", async()=>{
-    let response = await fetch('http://localhost:4000/api/workouts/');
+    let response = await fetch(`${host}/api/workouts/`);
     return response.json();
 })
 
 export const addWorkout = createAsyncThunk('addWorkout', async(workout)=>{
      
-    let response = await fetch('http://localhost:4000/api/workouts/createworkout', {
+    let response = await fetch(`${host}/api/workouts/createworkout`, {
         method: "POST",
         headers:{
             "Content-Type": "application/json"
@@ -15,8 +17,18 @@ export const addWorkout = createAsyncThunk('addWorkout', async(workout)=>{
         body: JSON.stringify(workout)
           
     });
-    return await response.json();
-    
+    return  response.json();
+
+})
+
+export const updateWorkout = createAsyncThunk('updateWorkout', async(id, updatedWorkout)=>{
+    let response = await fetch(`${host}api/workouts/${id}`, {
+        method: "PATCH",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(updatedWorkout)
+    });
+    return response.json();  
+
 })
 
 export const workoutSlice = createSlice({
@@ -26,14 +38,13 @@ export const workoutSlice = createSlice({
     },
     extraReducers: (builder)=>{
         builder.addCase(fetchWorkouts.fulfilled, (state, action)=>{
+            console.log(action)
             state.workouts = action.payload;
         })
         builder.addCase(addWorkout.fulfilled, (state, action)=>{
             state.workouts.unshift(action.payload)
         })
-        builder.addCase(addWorkout.rejected, (state, action)=>{
-            console.log("Error occured!!")
-        })
+       
     }
 })
 
