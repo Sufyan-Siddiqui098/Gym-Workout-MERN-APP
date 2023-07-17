08e199ffedfe93fba628cd72c820eccfcc64ab25
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-17 17:56:28 +0500

    function for modal is created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import WorkoutCard from '../components/WorkoutCard'
 import WorkoutForm from '../components/WorkoutForm'
+import UpdateModal from '../components/UpdateModal'
 
 const Home = () => {
+  const modalRef = useRef()
+  const handleClick = (e)=>{
+    if(e.target.id==='modal'){
+      return modalRef.current.classList.remove("active")
+    }
+  }
   return (
     <>
     <div className='container'>
@@ -15,6 +22,9 @@ const Home = () => {
             <WorkoutForm/>
           </div>
         </div>
+    </div>
+    <div className="modal" id='modal' ref={modalRef}  onClick={handleClick}>
+        <UpdateModal/>
     </div>
     </>
   )
