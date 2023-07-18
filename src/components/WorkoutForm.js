7b73e9a32aa5f02333b841807b17973e3747bf06
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-18 23:33:34 +0500

    Correction on required attribute

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -23,16 +23,16 @@ const WorkoutForm = () => {
         <h1>Add Workout</h1>
         <div>
             <label htmlFor="title">Title:</label>
-            <input type="text" name="title" id="title" required='true' placeholder='Title' onChange={(e)=>setTitle(e.target.value)} value={title} />
+            <input type="text" name="title" id="title" required={true} placeholder='Title' onChange={(e)=>setTitle(e.target.value)} value={title} />
         </div>
         <div>
             <label htmlFor="reps">Reps:</label>
-            <input type="number" name="reps" id="reps" required='true' placeholder='Reps' onChange={(e)=>setReps(e.target.value)} value={reps} />
+            <input type="number" name="reps" id="reps" required={true} placeholder='Reps' onChange={(e)=>setReps(e.target.value)} value={reps} />
         </div>
 
         <div>
             <label htmlFor="laod">Load (kg):</label>
-            <input type="number" name="laod" id="load" required='true' placeholder='Load (Kg)' onChange={(e)=>setLoad(e.target.value)} value={load}/>
+            <input type="number" name="laod" id="load" required={true} placeholder='Load (Kg)' onChange={(e)=>setLoad(e.target.value)} value={load}/>
         </div>
 
         <button className='btn submit'  >Add</button>
