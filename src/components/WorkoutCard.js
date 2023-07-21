4ebe643020ae87df53a396ca913f5c049213357d
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-21 19:18:43 +0500

    some changes

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchWorkouts, deleteWorkout, fetchWorkout } from '../store/workoutSlice'
+import {formatDistanceToNow} from 'date-fns'
 
 
 
@@ -29,11 +30,12 @@ const WorkoutCard = () => {
         <h3>{el.title}</h3>
         <div className="icons">
         <i className="fa-regular fa-pen-to-square" onClick={()=>{handleEdit(); return dispatch(fetchWorkout(el._id))}}></i>
-        <i className="fa-solid fa-trash" onClick={()=>{console.log(el._id);return dispatch(deleteWorkout(el._id))}}></i>
+        <i className="fa-solid fa-trash" onClick={()=>{return dispatch(deleteWorkout(el._id))}}></i>
         </div>
         </div>
         <p>{el.reps} Reps</p>
         <p>{el.load} Kg</p>
+        <p className='createdAt'>{formatDistanceToNow(new Date(el.createdAt), {addSuffix: true})}</p>
     </div>
     )
  }))
